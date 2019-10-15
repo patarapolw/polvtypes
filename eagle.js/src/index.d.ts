7701ad7e2df6024651a1a21e7cf54b645d5f5381
patarapolw <patarapolw@gmail.com>
2019-10-15 21:09:02 +0700

    publish

--- eagle.js/src/index.d.ts
+++ eagle.js/src/index.d.ts
@@ -8,19 +8,64 @@ declare module "eagle.js" {
     use: (it: any, options?: any) => void;
   };
 
-  @Component
+  @Component({
+    components: {Slide}
+  })
   export class Slideshow extends Vue {
+    /**
+     * @default 1
+     */
     @Prop() firstSlide: number;
+    /**
+     * @default null
+     */
     @Prop() lastSlide: number | null;
+    /**
+     * @default 1
+     */
     @Prop() startStep: number;
+    /**
+     * Navigate with mouse click or scroll event
+     * @default true
+     */
     @Prop() mouseNavigation: boolean;
+    /**
+     * Navigate with keyboard
+     * @default true
+     */
     @Prop() keyboardNavigation: boolean;
+    /**
+     * @default false
+     */
     @Prop() embedded: boolean;
+    /**
+     * @default false
+     */
     @Prop() inserted: boolean;
-    @Prop() onStartExit: boolean;
-    @Prop() onEndExit: boolean;
+    /**
+     * event callback for exiting slideshow through first slide
+     * @default null
+     */
+    @Prop() onStartExit: (() => void) | null;
+    /**
+     * event callback for exiting slideshow through last slide
+     * @default null
+     */
+    @Prop() onEndExit: (() => void) | null;
+    /**
+     * slideshow navigates back by step by default
+     * @default false
+     */
     @Prop() backBySlide: boolean;
+    /**
+     * go to first slide automatically when reaching the last one
+     * @default false
+     */
     @Prop() repeat: boolean;
+    /**
+     * alt + click can zoom on slide
+     * @default true
+     */
     @Prop() zoom: boolean;
 
     currentSlideIndex: number;
@@ -37,5 +82,58 @@ declare module "eagle.js" {
     nextSlide(): void;
     previousSlide(): void;
     changeDirection(direction: "prev" | "next"): void;
+  }
+
+  @Component
+  export class Slide extends Vue {
+    /**
+     * @default false
+     */
+    @Prop() skip: boolean;
+    /**
+     * Default enter animation
+     * @default null
+     */
+    @Prop() enter: string | null;
+    /**
+     * Enter animation for prev direction
+     * @default null
+     */
+    @Prop() enterPrev: string | null;
+    /**
+     * Enter animation for next direction
+     * @default null
+     */
+    @Prop() enterNext: string | null;
+    /**
+     * Default leave animation
+     * @default null
+     */
+    @Prop() leave: string | null;
+    /**
+     * Leave animation for prev direction
+     * @default null
+     */
+    @Prop() leavePrev: string | null;
+    /**
+     * Leave animation for next direction
+     * @default null
+     */
+    @Prop() leaveNext: string | null;
+    /**
+     * Total steps for this slide
+     * @default 1
+     */
+    @Prop() steps: number;
+    /**
+     * Navigate with mouse click or scroll event
+     * @default true
+     */
+    @Prop() mouseNavigation: boolean;
+    /**
+     * Navigate with keyboard
+     * @default true
+     */
+    @Prop() keyboardNavigation: boolean;
   }
 }
--- url-safe-string/src/index.d.ts
+++ url-safe-string/src/index.d.ts
@@ -8,10 +8,28 @@ declare module "url-safe-string" {
 
   namespace UrlSafeString {
     interface UrlSafeStringOptions {
+      /**
+       * truncates beyond maxLen
+       * @default 100
+       */
       maxLen?: number;
+      /**
+       * @default true
+       */
       lowercaseOnly?: boolean;
+      /**
+       * matches opposite of [a-z0-9]
+       * @default /((?!([a-z0-9])).)/gi
+       */
       regexRemovePattern?: RegExp;
+      /**
+       * e.g. - may be: '-', '_', '#'
+       * @default "-"
+       */
       joinString?: string;
+      /**
+       * @default true
+       */
       trimWhitespace?: boolean;
     }
   }
