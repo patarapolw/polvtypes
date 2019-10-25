a87437a144a4ca1e10eb40a59129b6f1c05eaa07
patarapolw <patarapolw@gmail.com>
2019-10-25 11:45:51 +0700

    update Reveal.js

--- reveal.js/tests/index.spec.ts
+++ reveal.js/tests/index.spec.ts
@@ -1,6 +1,9 @@
-import Reveal from "reveal.js";
+import Reveal, { RevealStatic } from "reveal.js";
 
 Reveal.initialize();
 Reveal.initialize({
   shuffle: true
 });
+
+const reveal = (window as any).Reveal as RevealStatic;
+reveal.configure({});
